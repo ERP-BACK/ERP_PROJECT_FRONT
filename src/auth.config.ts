e2c0c28c48b8e56506e7e36056cae2cce7ae7f32
Carlos8992 <cvelasco@saludelectronica.com>
2026-02-12 20:29:24 -0500

    feat: agregar configuración de autorización y token para Keycloak en la autenticación

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -7,6 +7,11 @@ export default {
             clientId: process.env.KEYCLOAK_ID!,
             clientSecret: process.env.KEYCLOAK_SECRET!,
             issuer: process.env.KEYCLOAK_ISSUER!,
+            authorization: {
+                url: `${process.env.NEXT_PUBLIC_KEYCLOAK_URL}/realms/${process.env.NEXT_PUBLIC_KEYCLOAK_REALM}/protocol/openid-connect/auth`,
+                params: { scope: "openid profile email" },
+            },
+            token: `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`,
         }),
     ],
 } satisfies NextAuthConfig;
